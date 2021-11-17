663635ab025901d04e37732d9918651dca0a790d
Pawikoski <pawikoski@gmail.com>
2021-11-17 23:54:53 +0100

    first init, scraping grades and counting average

---
+++ content.js
@@ -0,0 +1,42 @@
+currentUrl = $(location).attr('href');
+
+if(currentUrl.includes('przegladaj_oceny/uczen')) {
+    callback();
+}
+
+
+
+function callback() {
+    grades = Array();
+    let re = "^[0-9][.][0-9]{2}$";
+
+    
+    cells = document.querySelectorAll("td.right");
+    for (i=0; i<cells.length; i++){
+
+        let cell = cells[i].textContent;
+
+        if (cell != null){
+            if(cell.match(re)){
+                grades.push(parseFloat(cell));
+                i+=2;
+            }
+        }
+    }
+
+    function sum(arr) {
+        let a = 0
+        for(i=0; i<arr.length; i++) {
+            a += arr[i];
+        }
+        return a
+    }
+
+    let avg = Math.round((sum(grades) / grades.length) * 100) / 100
+
+
+    
+    // rows.concat(document.querySelectorAll("tr.line1"));
+    console.log(grades);
+    console.log(avg);
+}
